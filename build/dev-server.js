3df534184e9d43f6c5853eafc2e10b05f8bf37c3
huangkaoya <chdhyc@foxmail.com>
2025-01-18 16:01:56 +0800

    build: 回退node16相关的polyfill

--- build/dev-server.js
+++ build/dev-server.js
@@ -29,7 +29,6 @@
  *
  * Copyright © 2012-2019 Tencent BlueKing. All Rights Reserved. 蓝鲸智云 版权所有
  */
-require('web-streams-polyfill/polyfill')
 const path = require('path')
 const express = require('express')
 const webpack = require('webpack')
